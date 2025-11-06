2bd2abb6f43e5287f60add3a233c2e5f8ea59ca1
MelihSoezen <sozenm45@gmail.com>
2025-11-06 19:37:18 +0100

    Projektstruktur mihilfe von Spring initialzr erstellt und docker-compose.yml Datei noch hinzugefügt. Diese Datei ist wichtig für die erstellung eines postgres Container in docker. Mit dem Befehl "docker compose up -d " kann man einen docker container erstellen.

---
+++ src/main/java/com/Foerderportal/Backend/TestController.java
@@ -0,0 +1,14 @@
+package com.Foerderportal.Backend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "Backend läuft";
+    }
+}
