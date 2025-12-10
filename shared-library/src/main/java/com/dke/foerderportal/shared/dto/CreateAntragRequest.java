08dc9878e32dcf38d599cfff12980a85bfebd095
Drezem <troebinger.felician@gmail.com>
2025-12-10 12:37:48 +0100

    User Story 1 implementiert in Version 1.0

--- shared-library/src/main/java/com/dke/foerderportal/shared/dto/CreateAntragRequest.java
+++ shared-library/src/main/java/com/dke/foerderportal/shared/dto/CreateAntragRequest.java
@@ -1,19 +1,26 @@
 package com.dke.foerderportal.shared.dto;
 
+import com.dke.foerderportal.shared.model.AntragStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import jakarta.validation.constraints.NotNull;
 
 @Data
+@AllArgsConstructor
 public class CreateAntragRequest {
 
+    private Long id;
     @NotNull
     private String titel;
     private String beschreibung;
     @NotNull
     private BigDecimal betrag;
-    @NotNull
-    private Long antragstellerId;
+    private AntragStatus status;
+    private LocalDateTime eingereichtAm;
+    private String antragstellerName;
 }
